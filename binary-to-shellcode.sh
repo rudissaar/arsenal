e12775a65f389de343752654dd9056bceb99b7f2
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-01-06 19:51:04 +0000

    Added binary-to-shellcode.sh script.

---
+++ binary-to-shellcode.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Script that outputs contents of binary or object as shellcode.
+
+if [[ ${#} -lt 1 ]]; then
+    echo "> Usage: ${0} [Binary|Object]"
+    echo '> Exiting.'
+    exit 1
+fi
+
+BINARY="${1}"
+
+if [[ ! -f ${BINARY} ]]; then
+    echo "> Specified Binary|Object doesn't exist."
+    echo '> Exiting.'
+    exit 1
+fi
+
+for i in $(objdump -d ${BINARY} -M intel | grep '^ ' | cut -f2)
+do
+    echo -n "\x${i}"
+done
+
+echo
+
