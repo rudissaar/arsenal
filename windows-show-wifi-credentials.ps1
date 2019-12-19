7cfd6f454a765bf0050b344cff88c10fc6b704d9
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-12-19 12:14:10 +0000

    Added windows-show-wifi-credentials.ps1 script.

---
+++ windows-show-wifi-credentials.ps1
@@ -0,0 +1,33 @@
+﻿Set-StrictMode -Version 3
+
+$Output = (netsh wlan show profiles)
+
+[System.Collections.ArrayList] $ProfileNames = @()
+[System.Collections.ArrayList] $Results = @()
+
+ForEach ($Line in $Output)
+{
+    $Line = $Line -Match 'All User Profile\s+:\s+(.+)$'
+    
+    If ($Line) {
+        $ProfileNames.Add($Matches.Item(1)) > $Null
+    }
+}
+
+ForEach ($ProfileName in $ProfileNames)
+{
+    $Output = (netsh wlan show profile name="$ProfileName" key=clear)
+    
+    ForEach ($Line in $Output) {
+        $Line = $Line -Match 'Key Content\s+:\s+(.+)$'
+
+        If ($Line) {
+            $Password = ${Matches}.Item(1)
+            $Results.Add(@{"${ProfileName}"="${Password}"}) > $Null
+            Break
+        }
+
+    }
+}
+
+Write-Output $Results
